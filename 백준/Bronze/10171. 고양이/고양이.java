3564f111be08d28eec030125491a7033517aa703
kimdoeun <158231909+ide05484@users.noreply.github.com>
2024-03-04 15:35:25 +0900

    [Bronze V] Title: 고양이, Time: 112 ms, Memory: 13848 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		String str = "\\    /\\\n"
+				+ " )  ( ')\n"
+				+ "(  /  )\n"
+				+ " \\(__)|";
+				
+		System.out.println(str);
+
+	}
+}
