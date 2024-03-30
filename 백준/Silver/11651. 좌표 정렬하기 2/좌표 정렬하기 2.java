6b82d9bf049b2ccd060b4955cdf5427fbc2ee147
kimdoeun <158231909+ide05484@users.noreply.github.com>
2024-03-30 22:45:49 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2116 ms, Memory: 192076 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static class Dot implements Comparable<Dot> {
+		int x;
+		int y;
+
+		public Dot(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int compareTo(Dot o) {
+			if (this.y == o.y) { // 같다면 x 좌표로 해줘
+				return this.x- o.x;
+			}
+			return this.y - o.y;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt(); // 좌표받을 갯수
+		Dot[] dot = new Dot[N]; // 좌표가 들어갈 배열
+
+		for (int n = 0; n < N; n++) {
+			dot[n] = new Dot(sc.nextInt(), sc.nextInt()); // 좌표받아줌
+		}
+
+		Arrays.sort(dot);
+
+		for (int n = 0; n < N; n++) {
+			System.out.println(dot[n].x + " " + dot[n].y);
+		}
+	}
+}
