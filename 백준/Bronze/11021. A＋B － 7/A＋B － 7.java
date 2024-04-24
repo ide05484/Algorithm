67cdfc7c881ff1f093636839a1de4b85821c881c
kimdoeun <158231909+ide05484@users.noreply.github.com>
2024-04-24 21:59:13 +0900

    [Bronze V] Title: A+B - 7, Time: 256 ms, Memory: 18652 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt(); // 테스트케이스 수
+
+		for (int tc = 1; tc <= T; tc++) {
+
+			int A = sc.nextInt();
+			int B = sc.nextInt(); // 두 수 주어짐
+
+			System.out.println("Case #" + tc + ": " +(A + B));
+
+		} // 테케마감
+	}// main
+}
