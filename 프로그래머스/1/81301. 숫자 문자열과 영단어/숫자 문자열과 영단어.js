bd32b3e9b7434b2625472655a09b685cea9f65ef
kimdoeun <158231909+ide05484@users.noreply.github.com>
2024-07-12 08:28:06 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,30 @@
+function solution(s) {
+    
+    let answer = new String();
+    let numWord = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    
+    let changeNum = new String();
+    
+    for(let i = 0; i < s.length; i++){
+        let c = s.charAt(i);
+        
+        if(c === '0' || c === '1' ||c === '2'||c === '3'||c === '4'||c === '5'||c === '6'||c === '7'||c === '8'||c === '9') {
+            //숫자라면
+            answer+=c;
+        } else {
+            //숫자가 아니라면
+            changeNum += c;
+            
+            for(let n = 0; n < 10; n++){
+                if(changeNum === numWord[n]) {
+                    //같은게 있다면!!
+                    answer += n.toString(); // 문자열로 변환한 값을 넣어주셈~!
+                    changeNum = new String(); //다시 빈문자열로
+                }
+            }
+            
+        }
+    }
+
+    return Number(answer); //숫자로 형 변환해줘야 함 ....
+}
