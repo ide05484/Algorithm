fd821e5c20d5df9312c6fbcac0cc0c9a6a4e6a83
kimdoeun <158231909+ide05484@users.noreply.github.com>
2024-07-13 23:10:14 +0900

    [level 1] Title: 삼총사, Time: 0.19 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,16 @@
+function solution(number) {
+    
+    let count = 0;
+    
+    for(let i = 0; i <= number.length - 3; i++) {
+        for(let j = i+1; j <= number.length - 2; j++){
+            for(let k = j+1; k <= number.length - 1; k++){
+                if(number[i]+number[j] + number[k] === 0) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
+}
