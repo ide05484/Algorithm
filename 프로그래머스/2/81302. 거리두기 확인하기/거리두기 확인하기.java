ffc551bf4e5b4cb436c8bd50b3170d00a4ca19a1
kimdoeun <158231909+ide05484@users.noreply.github.com>
2025-04-15 21:36:27 +0900

    [level 2] Title: 거리두기 확인하기, Time: 4.92 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -10,86 +10,86 @@ class Solution {
     public int[] solution(String[][] places) {
         
         int T = places.length;
+        
         int[] answer = new int[T];
         
         to: for(int t = 0; t < T; t++) {
-            char[][] room = new char[5][5];
-            String[] tmp = places[t];
-        
-            for(int r = 0; r < 5; r++) {
-                for(int c = 0; c < 5; c++) {
-                    room[r][c] = tmp[r].charAt(c);                
-                }
-            }
             
-            //여기에서 체크
+            char[][] room = new char[5][5];
+            
+            for(int r = 0; r < 5; r++) {
+                String str = places[t][r];
+                for(int c = 0; c < 5; c++) {
+                    room[r][c] = str.charAt(c);
+                }
+            } //입력 완료
+            
             for(int r = 0; r < 5; r++) {
                 for(int c = 0; c < 5; c++) {
                     if(room[r][c] == 'P') {
                         if(!bfs(room, r, c)) {
-                            //만약에 불가능하면
                             answer[t] = 0;
                             continue to;
                         };
                     }
                 }
-            }
+            } //bfs
             
             answer[t] = 1;
             
-        }
+        } //테케돌리기
         
         return answer;
     }
     
     public static boolean bfs(char[][] room, int R, int C) {
-        boolean[][] visit = new boolean[5][5];
+        
         Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visit = new boolean[5][5];
         
         queue.add(new int[]{R, C});
         visit[R][C] = true;
         
         while(!queue.isEmpty()) {
             
-            int[] tmp = queue.poll();
-            int r = tmp[0];
-            int c = tmp[1];
+            int[] now = queue.poll();
+            int r = now[0];
+            int c = now[1];
             
-            int dist = Math.abs(r - R) + Math.abs(c - C); //거리
+            int dist = Math.abs(r - R) + Math.abs(c - C);
             
-            if(dist > 2) continue; 
+            //자기자신이거나 거리가 멀면 하지 않는다
+            if(dist > 2) continue;
             
-            if(dist == 1 && room[r][c] == 'P') return false;  // 거리 1이면 무조건 위반
-
-            if(dist == 2 && room[r][c] == 'P') { // 거리가 2일 때 파티션 여부 체크
-                    // 같은 행
-                    if(r == R) {
-                        if(room[r][(c + C) / 2] == 'O') return false;
-                    }
-                    // 같은 열
-                    else if(c == C) {
-                        if(room[(r + R) / 2][c] == 'O') return false;
-                    }
-                    // 대각선
-                    else {
-                        if(room[r][C] == 'O' || room[R][c] == 'O') return false;
-                    }
+            if(dist == 1) {
+                if(room[r][c] == 'P') return false;
+            } else if(dist == 2 && room[r][c] == 'P') {
+                //일직선일때
+                if(r == R) {
+                    if(room[r][(c+C)/2] == 'O') return false; //칸막이 없으면
+                }
+                
+                if(c == C) {
+                    if(room[(r+R)/2][c] == 'O') return false; //칸막이 없으면
+                }
+                
+                //대각선일때
+                if(room[r][C] == 'O' || room[R][c] == 'O') return false;
             }
-  
+            
             for(int d = 0; d < 4; d++) {
                 int nr = r + dr[d];
                 int nc = c + dc[d];
                 
                 if(nr < 0 || nr >= 5 || nc < 0 || nc >= 5 || visit[nr][nc]) continue;
                 
-                if(room[nr][nc] != 'X') {
-                    queue.add(new int[]{nr, nc});
-                    visit[nr][nc] = true;
-                }
+                queue.add(new int[]{nr, nc});
+                visit[nr][nc] = true;
             }
             
         }
         
         return true;
     }
+    
 }
