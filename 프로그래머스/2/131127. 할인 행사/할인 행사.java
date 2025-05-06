71dfd9165d55c6fac7d3553cff7b20bbf95563c4
kimdoeun <158231909+ide05484@users.noreply.github.com>
2025-05-06 22:17:36 +0900

    [level 2] Title: 할인 행사, Time: 86.85 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,39 +3,32 @@ import java.io.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
+    
+        Map<String,Integer> map = new HashMap<>();
         
-        Map<String, Integer> wishList = new HashMap<>(); //위시리스트
+        int len = want.length;
         
-        for(int i = 0; i < want.length; i++) {
-            wishList.put(want[i], number[i]);
+        for(int i = 0; i < len; i++) {
+            map.put(want[i], number[i]);
         }
         
-        int len = discount.length;
+        int count = 0;
         
-        int answer = 0;
-        
-        to: for(int i = 0; i <= len - 10; i++) {
-            
-            Map<String, Integer> canBuy = new HashMap<>();
-            
-            for(int j = i; j < i + 10; j++) {
-                canBuy.put(discount[j], canBuy.getOrDefault(discount[j], 0) + 1);
+        //할인날짜를 돌면서 10일간의 체크를 함
+        to: for(int i = 0; i <= discount.length - 10; i++) {
+            Map<String, Integer> tmp = new HashMap<>();
+            for(int j = i; j < i+10; j++) {
+                tmp.put(discount[j], tmp.getOrDefault(discount[j], 0) + 1);
             }
             
-            //여기에서 숫자 일치하는지 확인
-            for(int w = 0; w < want.length; w++) {
-                String product = want[w];
-                
-                if(wishList.get(product) != canBuy.getOrDefault(product, 0)) {
-                    //하나라도 안맞으면
-                    continue to;
-                }
+            //여기에서 수량 다 샀는지 체크
+            for(int l = 0; l < len; l++) {
+                if(map.get(want[l]) != tmp.get(want[l])) continue to;
             }
             
-            //다맞으면
-            answer++;
+            count++;
         }
         
-        return answer;
+        return count;
     }
 }
