3f933d8fa0aabe8ac7682d23523dd3159410768e
kimdoeun <158231909+ide05484@users.noreply.github.com>
2026-05-08 22:26:18 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,10 @@
+-- 코드를 작성해주세요
+# 스킬코드는 2의 제곱수로 구성 ..... BIT
+# ID 기준 오름차순
+
+SELECT DISTINCT D.ID, D.EMAIL, D.FIRST_NAME, D.LAST_NAME
+FROM DEVELOPERS D
+JOIN SKILLCODES S
+ON (D.SKILL_CODE & S.CODE) != 0 # 비트 계산
+WHERE S.CATEGORY = 'Front End'
+ORDER BY D.ID
