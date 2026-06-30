8474f4070e9dbea08f74d4afd8712bca6b789142
kimdoeun <158231909+ide05484@users.noreply.github.com>
2026-06-30 22:34:18 +0900

    [level 2] Title: 할인 행사, Time: 37.16 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -4,29 +4,22 @@ import java.io.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
     
-        Map<String,Integer> map = new HashMap<>();
+        Map<String, Integer> todo = new HashMap<>();
         
-        int len = want.length;
-        
-        for(int i = 0; i < len; i++) {
-            map.put(want[i], number[i]);
+        for(int i = 0; i < want.length; i++) {
+            todo.put(want[i], number[i]);
         }
         
         int count = 0;
         
-        //할인날짜를 돌면서 10일간의 체크를 함
-        to: for(int i = 0; i <= discount.length - 10; i++) {
+        for(int i = 0; i <= discount.length - 10; i++) {
             Map<String, Integer> tmp = new HashMap<>();
-            for(int j = i; j < i+10; j++) {
-                tmp.put(discount[j], tmp.getOrDefault(discount[j], 0) + 1);
+            
+            for(int d = 0; d < 10; d++) {
+                tmp.put(discount[i+d], tmp.getOrDefault(discount[i+d], 0) + 1);
             }
             
-            //여기에서 수량 다 샀는지 체크
-            for(int l = 0; l < len; l++) {
-                if(map.get(want[l]) != tmp.get(want[l])) continue to;
-            }
-            
-            count++;
+            if(todo.equals(tmp)) count++;            
         }
         
         return count;
