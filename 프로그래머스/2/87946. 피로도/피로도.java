d4c47fa181d17ce8ec6aa841a484abcb763a5cfb
kimdoeun <158231909+ide05484@users.noreply.github.com>
2026-07-02 22:00:29 +0900

    [level 2] Title: 피로도, Time: 1.56 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,53 +1,31 @@
-import java.awt.Point;
-
 class Solution {
     
-    static int max; //최대 던전수
+    static int max;
     
     public int solution(int k, int[][] dungeons) {
-        // 순열로 순서를 만든 후 모든 탐색 갈기자
-		boolean[] visit = new boolean[dungeons.length];
-		
-		Point[] select = new Point[dungeons.length]; // 던전을 돌 순서 배열
-		max = 0;
-		perm(k, dungeons, visit, 0, select);
-
-		return max;
+        
+        max = 0;
+        
+        boolean[] visit = new boolean[dungeons.length];
+        
+        dfs(visit, k, dungeons, 0);
+        
+        return max;
     }
     
-    private static void perm(int k, int[][] dungeons, boolean[] visit, int idx, Point[] select) {
-		if (idx == select.length) {
-			// 순열을 만들었다면 여기서 계산
-			go(k, select);
-			return;
-		}
-		
-		for (int r = 0; r < dungeons.length; r++) {
-			//던전순서를 정하자
-			if(!visit[r]) {
-				//방문안했다면, 그곳 방문쳌
-				visit[r] = true;
-				select[idx] = new Point(dungeons[r][0], dungeons[r][1]);
-				perm(k, dungeons, visit, idx+1, select);
-				
-				//다시 나올때 원복
-				visit[r] = false;
-			}
-		}
-	}
-
-	private static void go(int k, Point[] select) {
-		
-		int count = 0;
-		
-		for(int i = 0; i < select.length; i++) {
-			if(select[i].x <= k) {
-				//들어갈 수 있는 최소 피로도
-				count++;
-				k -= select[i].y; //소모된거만큼 뺴준다
-			}
-		}
-		
-		max = Math.max(max, count);
-	}
+    //방문배열, 체력, 던전, 돌았던 던전 개수
+    public void dfs(boolean[] visit, int k, int[][] dungeons, int cnt) {
+        max = Math.max(max, cnt);
+        
+        for(int d = 0; d < dungeons.length; d++) {
+            if(visit[d] || k < dungeons[d][0]) continue;
+            //방문했거나 못가는 던전이면 패스
+            
+            visit[d] = true;
+            dfs(visit, k - dungeons[d][1], dungeons, cnt + 1);
+            visit[d] = false; //원상복구
+        }
+        
+        return;
+    }
 }
