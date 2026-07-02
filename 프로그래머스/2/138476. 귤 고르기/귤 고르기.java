afaf1991bfa674dc788c03d7f21784d0bf6f1656
kimdoeun <158231909+ide05484@users.noreply.github.com>
2026-07-02 21:21:10 +0900

    [level 2] Title: 귤 고르기, Time: 34.80 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -6,21 +6,18 @@ class Solution {
         
         Map<Integer, Integer> map = new HashMap<>();
         
-        int len = tangerine.length;
-        
-        for(int i = 0; i < len; i++) {
+        for(int i = 0; i < tangerine.length; i++) {
             map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
         }
         
         List<Integer> list = new ArrayList<>(map.values());
         list.sort(Collections.reverseOrder());
         
-        
         int cnt = 0;
         
         for(int i = 0; i < list.size(); i++) {
             k -= list.get(i);
-            cnt ++;
+            cnt++;
             
             if(k <= 0) break;
         }
